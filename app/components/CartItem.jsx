69c7b9d7e3879ce1e9ba25c195da936529a71627
Dzhanaur1 <3923328@gmail.com>
2023-11-03 12:42:56 +0300

    fix

--- app/components/CartItem.jsx
+++ app/components/CartItem.jsx
@@ -11,7 +11,7 @@ const CartItem = (product) => {
       <div className="flex-[2]">
         <Link href={`catalog/item/${product.id}`}>
           <img
-            src={product.image}
+            src={product?.image}
             alt="Product Image"
             className=" object-cover rounded"
           />
@@ -19,13 +19,13 @@ const CartItem = (product) => {
       </div>
       <div className=" flex-[2] pl-4">
         <h2 className="text-sm lg:text-lg font-semibold mb-2">
-          {product.name}
+          {product?.name}
         </h2>
         <p className="text-gray-600 lg:text-sm text-[11px] mb-1">
           Размеры:
           <br />
-          {product.Description.lenght} x {product.Description.width} x
-          {product.Description.height}
+          {product?.Description?.lenght} x {product?.Description?.width} x
+          {product?.Description?.height}
         </p>
         <p className="text-gray-600 lg:text-sm text-[11px] mb-1">
           Материал: Лиственница
@@ -39,7 +39,7 @@ const CartItem = (product) => {
           -
         </button>
         <p className="px-2 lg:px-4 py-1 rounded-lg  bg-neutral-300">
-          {product.count}
+          {product?.count}
         </p>
         <button
           onClick={() => dispatch(addItem(product))}
