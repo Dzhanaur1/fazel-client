0854a3b20b02d66f6404a8fc3b7a831653c60f5f
Dzhanaur1 <3923328@gmail.com>
2023-11-06 11:40:21 +0300

    fix metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,18 +8,15 @@ import { ReduxProvider } from "@/redux/provider";
 
 const inter = Noto_Sans({ subsets: ["cyrillic"], weight: "400" });
 
-// export const metadata = {
-//   title:
-//     " Купить Детские площадки - компания по производсту уличного оборудования «Fazel» | Fazel",
-//   description:
-//     "Рекомендуем купить детские игровые городки и площадки у нас в профильной компании «Детские площадки» в Краснодаре. Осуществляем по желанию Клиента работы по бетонированию и качественную установку под ключ. Предоставляем длительную гарантию и быструю доставку.",
-// };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className=" h-full">
       <head>
-        <link rel="icon" href="/icons/Fazel.svg" type="image/svg+xml" />
+        <link
+          rel="icon"
+          href="https://fazel-client.vercel.app/icons/Fazel.svg"
+          type="image/svg+xml"
+        />
         <meta name="yandex-verification" content="04f7e6e6d5b9a2bb" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="lang" content="ru" />
